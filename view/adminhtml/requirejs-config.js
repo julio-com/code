38fdfc6f7a28c9aecf259bc2428d19d7a113534f
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-29 02:28:18 +0600

    0.0.3: https://github.com/julio-com/core/issues/3

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,7 @@
+// 2019-07-29
+// «Implement a video control on product pages similar to asos.com»
+// https://github.com/julio-com/core/issues/3
+// https://www.upwork.com/ab/f/contracts/22583013
+var config = {config: {mixins: {
+	'Magento_ProductVideo/js/get-video-information': {'Julio_Core/get-video-information': true}
+}}};
