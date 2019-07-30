5db95e52bddca5d399b261f5cc4d063ddeec2f93
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-30 20:50:11 +0600

    no message

---
+++ view/frontend/web/main.js
@@ -0,0 +1,10 @@
+// 2019-07-30
+define(['jquery', 'domReady!'], function($) {return (
+/**
+ * @param {Object} c
+ * @param {HTMLDivElement} e
+ */
+function(c, e) {
+	var $e = $(e);
+	debugger;
+});});
