1e7e88c2c0a70c0ef593a473b6e5e061e8353ca5
Dmitry Fedyuk <dfediuk@gmail.com>
2019-08-01 21:07:01 +0600

    1.0.0

--- view/frontend/web/configurable.js
+++ view/frontend/web/configurable.js
@@ -5,14 +5,12 @@ return function(sb) {
 $.extend(sb.prototype, {
 	generateHtmlImage: w.wrap(sb.prototype.generateHtmlImage, function(_super) {
 		var r = _super();
-		return $('<div>').append($('<video>')
-			.attr({class: 'jul-video', controls: true})
-			.append($('<source>').attr({
-				src: 'https://video.asos-media.com/products/ASOS/_media_/fb8/fb83c3ae-8dab-4f98-a0a9-e5eec6661487.mp4'
-				,type: 'video/mp4'
-			})))
-			.html()
-			+ r
+		var url = $('div.julio-video').data('video');
+		return !url ? r : $('<div>').append(
+				$('<video>')
+					.attr({class: 'jul-video', controls: true})
+					.append($('<source>').attr({src: url, type: 'video/mp4'}))
+			).html() + r
 		;
 	})
 });
--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,39 +1,38 @@
 // 2019-07-30
-define(['jquery', 'domReady!'], function($) {var ran = false; return (
+define(['jquery', 'df-scroll', 'domReady!'], function($) {return (
 /**
  * @param {Object} c
+ * @param {String} c.url
  * @param {HTMLDivElement} e
  */
 function(c, e) {
-	(function() {
-		if (!ran) {
-			ran = true;
-			var $g = $('.gallery-list');
-			var $img = $g.children('.product').first();
-			/*$g.prepend(
-				$('<video>').attr({
-					class: 'jul-video'
-					,controls: true
-					,height: $img.height()
-					,width: $img.width()
-				}).append($('<source>').attr({
-					src: 'https://video.asos-media.com/products/ASOS/_media_/fb8/fb83c3ae-8dab-4f98-a0a9-e5eec6661487.mp4'
-					,type: 'video/mp4'
-				}))
-			);*/
+	var $e = $(e);
+	var $a = $('a', $e);
+	var is1Column = $e.hasClass('julio-video-1column');
+	var is2Columns = $e.hasClass('julio-video-2columns');
+	$a.toggleClass('df-hidden', !$e.data('video'));
+	// 2019-08-01 https://www.w3schools.com/howto/howto_js_fullscreen.asp
+	function fs(e) {
+		e.requestFullscreen ? e.requestFullscreen() : (
+			e.mozRequestFullScreen ? e.mozRequestFullScreen() : (
+				e.webkitRequestFullscreen ? e.webkitRequestFullscreen() : (
+					e.msRequestFullscreen ? e.msRequestFullscreen() : $(window).scrollTo('.breadcrumbs')
+				)
+			)
+		);
+	}
+	$a.click(function() {
+		if (is2Columns) {
+			$(window).scrollTo('.breadcrumbs');
 		}
-	})();
-	(function() {
-		var $e = $(e);
-		var $a = $('a', $e);
-		$a.click(function() {
-			$a.toggleClass('jul-active');
-			if ($a.hasClass('jul-active')) {
-
-			}
-			else {
-
-			}
-		});
-	})();
+		var c = 'jul-active';
+		$a.toggleClass(c);
+		var $v = $('video.jul-video'); // 2019-08-01 We can not cache this block.
+		var on = $a.hasClass(c);
+		$v.toggle(on);
+		if (is1Column) {
+			fs($v.get(0));
+		}
+		on ? $v.get(0).play() : $v.get(0).pause();
+	});
 });});
